ab90ccf2d8f9c4e2e452aae53553b5f799026bdc
Yolotzin-Araceli <61529907+Yolotzin-Araceli@users.noreply.github.com>
2020-02-26 22:04:49 -0600

    Add files via upload

---
+++ profil[1].py
@@ -0,0 +1,20 @@
+DIVIDER='--------------------------------------------------'
+ID='193110382'
+NAME='Yolotzin'
+LAST='Romero'
+AGE= 18
+ARTISTIC= True
+RIT=False
+ACRO= True
+
+print(DIVIDER)
+print ('{:10}{}'.format('ID: ', ID))
+print(DIVIDER)
+print ('{:10}{} {}'.format('NAME:  ',LAST,NAME))
+print ('{:10}{}'.format('AGE: ', AGE , 'years'))
+print(DIVIDER)
+print("SPORT:")
+print ('{:10}{}'.format('ARTISTIC GYMNASTICS: ', ARTISTIC))
+print ('{:10}{}'.format('RITHMIC GYMNASTICS: ', RIT))
+print ('{:10}{}'.format('ACROBATIC GYMNASTICS: ', ACRO))
+print(DIVIDER)
